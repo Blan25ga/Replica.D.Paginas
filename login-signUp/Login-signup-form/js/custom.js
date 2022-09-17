79021c3c99c9ab661a3819216ca1f295a85ed2bd
Blan25ga <gabriel.bl@hotmail.com.ar>
2022-09-17 15:19:41 -0300

    Se agregaron 2 nuevos formularios- login y sign-up

---
+++ login-signUp/Login-signup-form/js/custom.js
@@ -0,0 +1,20 @@
+(function() {
+
+    'use strict';
+
+    var elToggle = document.querySelector('.js-password-show-toggle'),
+        passwordInput = document.getElementById('password');
+
+        elToggle.addEventListener('click', (e) => {
+            e.preventDefault();
+
+            if ( elToggle.classList.contains('active') ) {
+                passwordInput.setAttribute('type', 'password');
+                elToggle.classList.remove('active');
+            } else {
+                passwordInput.setAttribute('type', 'text');
+                elToggle.classList.add('active');
+            }
+        })
+
+})()
